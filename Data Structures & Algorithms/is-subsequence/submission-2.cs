2109fe215c4233d90838ec75fab333e56973612b
Hassan Baig <113861427+MirzaHB@users.noreply.github.com>
2026-07-07 00:19:41 -0600

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        if(s.Length > t.Length) return false;
+        if(s.Length == 0) return true;
+        int sIndex = 0;
+        for(int i=0; i<t.Length; i++){
+            if(sIndex == s.Length) return true;
+            if(s[sIndex] == t[i]){
+                sIndex+=1;
+                if(sIndex == s.Length) return true;
+            }
+        }
+        return false;
+    }
+}
