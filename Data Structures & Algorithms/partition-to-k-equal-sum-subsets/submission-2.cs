edb283a1a27594d41bee956c23394a3db3c11c9a
Hassan Baig <113861427+MirzaHB@users.noreply.github.com>
2026-07-10 23:03:15 -0600

    Add: partition-to-k-equal-sum-subsets - submission-2

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-2.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool CanPartitionKSubsets(int[] nums, int k) {
+        int total = 0;
+        foreach(int n in nums) total+=n;
+        if(total%k!=0) return false;
+        int target = total/k;
+        Array.Sort(nums, (a,b) => b.CompareTo(a));
+        bool dfs(List<int> subsets, int index){
+            if(index == nums.Length){
+                foreach(int subset in subsets)
+                    if(subset != target) return false;
+                return true;
+            }
+            if(nums[index]> target) return false;
+
+            for(int i=0; i<k; i++){
+                if(subsets[i]+nums[index]<=target){
+                    subsets[i]+=nums[index];
+                    if(dfs(subsets, index+1)) return true;
+                    subsets[i]-=nums[index];
+                }
+                if (subsets[i] == 0) break; 
+            }
+            return false;
+        }
+        return dfs(new List<int>(new int[k]), 0);
+    }
+}
