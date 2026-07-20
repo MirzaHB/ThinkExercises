c9edbb7595e7b20977968cded46916853abd783b
Hassan Baig <113861427+MirzaHB@users.noreply.github.com>
2026-07-20 17:32:34 -0600

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        
+        int l=0;
+        int r = nums.Length-1;
+
+        while(r>=l){
+            int m = l+(r-l)/2;
+            if(nums[m]==target) return m;
+            if(nums[m]>target) r=m-1;
+            if(nums[m]<target) l=m+1;
+        }
+        return -1;
+    }
+}
