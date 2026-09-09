5194ef3a116a4db3ab8dde42cdeda70e9ac7e692
Hassan Baig <113861427+MirzaHB@users.noreply.github.com>
2026-09-09 14:30:08 -0600

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        var ans = new int[nums.Length * 2];
+        for(int i = 0; i < (nums.Length * 2); i++) {
+            ans[i] = nums[i % nums.Length];
+        }
+        return ans;
+    }
+}
