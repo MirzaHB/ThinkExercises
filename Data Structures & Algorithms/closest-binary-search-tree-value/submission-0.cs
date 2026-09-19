7914bf89a6913db9ba4b8b54ec74160af7d378d7
Hassan Baig <113861427+MirzaHB@users.noreply.github.com>
2026-09-19 17:08:44 -0600

    Add: closest-binary-search-tree-value - submission-0

---
+++ Data Structures & Algorithms/closest-binary-search-tree-value/submission-0.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int ClosestValue(TreeNode root, double target) {
+        double ansDiff = int.MaxValue;
+        int ans = 0;
+
+        void dfs(TreeNode curr){
+            if(curr==null) return;
+            if(ansDiff > Math.Abs((double)curr.val - target)){
+                ansDiff = Math.Abs((double)curr.val - target);
+                ans = curr.val;
+            }
+            dfs(curr.left);
+            dfs(curr.right);
+        }
+        dfs(root);
+        return ans;
+    }
+}
